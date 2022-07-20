65f3038dd5a7143e63568ab97cdab7d7713d73cd
Pedro Esparza <pesparza@lean-tech.io>
2022-07-20 15:02:44 -0500

    MVC API Restfull v1

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Turn On The Lights
+|--------------------------------------------------------------------------
+*/
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE');
+header('content-type: application/json; charset=utf-8');
+
+use src\api\controllers\RepositoryController;
+use src\api\controllers\CategoryController;
+use src\api\Helpers;
+
+spl_autoload_register(function ($class) {
+    $file = __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+    if (is_readable($file)) require __DIR__ . '/' . str_replace('\\', '/', $class) . '.php';
+});
+
+$jsondata = file_get_contents('php://input');
+$request = isset($jsondata) && !empty($jsondata) ? @json_decode($jsondata, TRUE) : array();
+
+if (isset($jsondata) && !empty($jsondata) && json_last_error() !== JSON_ERROR_NONE) {
+    Helpers::returnToAction(Helpers::formatResponse(404, 'Incorrect JSON Format', []));
+    return;
+}
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+*/
+
+$router = new src\core\Router();
+
+$router->get('/', RepositoryController::class . '::indexAction');
+
+$router->any(RepositoryController::class . '::noActionFound');
+
+$router->run($request, $_SERVER['REQUEST_METHOD']);
---
+++ src/api/Helpers.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace src\api;
+
+class Helpers
+{
+    public static array $modules = ['category', 'customer', 'supplier', 'item'];
+
+    public static function validModule($module): bool
+    {
+        return in_array($module, self::$modules);
+    }
+    public static function dye($value): void
+    {
+        echo '<pre>';
+        print_r($value);
+        echo '</pre>';
+        exit(1);
+    }
+
+    public static function formatResponse($status, $message, $data = []): array
+    {
+        return [
+            'status' => $status,
+            'message' => $message,
+            'data' => $data
+        ];
+    }
+
+    public static function returnToAction(array $response): void
+    {
+        echo json_encode($response, http_response_code($response['status']));
+    }
+
+    public static function getFirstKeyName(array $array)
+    {
+        $keys = array_keys($array);
+        return $keys[0];
+    }
+}
